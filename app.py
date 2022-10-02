7b1044c95eb707d25cecc139ce6998080005e29e
Kodai <motiviquestudio@gmail.com>
2022-10-02 05:40:28 -0500

    Survey minus session

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, request, redirect, flash, jsonify
+from flask_debugtoolbar import DebugToolbarExtension 
+from surveys import satisfaction_survey as satsurv
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "surv_sess"
+
+debug = DebugToolbarExtension(app)
+
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+responses = []
+
+print(responses)
+print("HelLO there.")
+
+@app.route("/")
+def root():
+    """Take user to homepage of survey."""
+    print(responses)
+    responses.clear()
+    print(responses)
+    title = satsurv.title
+    instr = satsurv.instructions
+    resp = len(responses)
+    return render_template('home.html', title = title, instr = instr, id = resp)
+
+#Below is the working before checking for index out of bounds
+# @app.route("/questions/<int:respid>")
+# def question01(respid):
+#     respid = len(responses) 
+#     print(respid)
+#     question = satsurv.questions[respid].question
+#     """Take user to homepage of survey."""
+#     return render_template('questions.html', qnum=respid + 1, question = question)
+
+#Below handles out of bounds test
+@app.route("/questions/<int:respid>")
+def question01(respid):
+    #The following line prevents a user from skipping a question by insuring the questin id is 
+    #based on the responses list rather than the url increment - if you try to visit a question
+    #out of order, notice the value of respid before and after the =len(responses)
+    print(respid)
+    #Notice below that if you try to visit a qeustion out of order, Whoa Nelly! prints to the
+    #server but we are still on the correct question. 
+    if respid != len(responses):
+        print("Whoa nelly!")
+        flash("Whoa Nelly!")
+    respid = len(responses)
+    question = satsurv.questions[respid].question
+    print(respid)
+    #The following line checks if the respid (which is based on the responses list) to the length of the 
+    #list of questions defined in the class satisfaction_survey, which, if true, will always render
+    #the thanks.html page without a redirect.
+    if respid == len(satsurv.questions):
+        print("true") 
+        return render_template('thanks.html')
+    elif respid > len(satsurv.questions):
+        return redirect('questions.html', qnum=respid + 1, question = question)
+
+    else:
+        print(respid, len(satsurv.questions))
+        # question = satsurv.questions[respid].question
+        """Take user to homepage of survey."""
+        return render_template('questions.html', qnum=respid + 1, question = question)
+
+@app.route("/answer", methods=["POST"])
+def append_to_responses():
+    """saves response in responses[] and proceeds to next question."""
+    answer = request.form['answer']
+    responses.append(answer)
+
+    # if (len(responses) == len(survey.questions)):
+    #     return redirect("/surv_complete")
+    # else:
+    print(responses)
+    return redirect(f"/questions/{len(responses)}")
